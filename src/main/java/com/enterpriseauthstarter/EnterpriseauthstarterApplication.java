bc66f1047efc06c927c94e442babe0baa6ef864c
Srinivasalu G <srinivasalu2107@gmai.com>
2026-05-09 21:54:17 +0530

    Initial Spring Boot setup with PostgreSQL and JWT dependencies

---
+++ src/main/java/com/enterpriseauthstarter/EnterpriseauthstarterApplication.java
@@ -0,0 +1,13 @@
+package com.enterpriseauthstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseauthstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseauthstarterApplication.class, args);
+	}
+
+}
